97a824f60ef47e757013b86a8beb76d123bb1a7b
CaluRo <151804212+CaluRo@users.noreply.github.com>
2023-12-08 00:58:17 -0600

    Se actualizan directorios

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
+ cursor: "<span style='color: #873b7c;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #fb4993;">Diseñadora de la comunicación gráfica.</span>')
+  .typeString('<span style="color: #873b7c;">Diseñadora de la comunicación gráfica.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
